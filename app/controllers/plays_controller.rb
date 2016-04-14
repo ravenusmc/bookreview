e14a1cd54d8bd60915f8d4ce81b6f61767a8edef
ravenusmc <mcuddy77@gmail.com>
2016-04-14 16:09:57 -0400

    added new action

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,5 +2,20 @@ class PlaysController < ApplicationController
 
   def index 
   end 
+
+  def new 
+    @play = Play.new 
+  end 
+
+  #ensures that the item is added to the database
+  def create
+    @play = Play.new(play_params)
+  end
+
+  private 
+
+  def play_params
+    params.require(:play).permit(:title, :description, :director)
+  end 
   
 end
